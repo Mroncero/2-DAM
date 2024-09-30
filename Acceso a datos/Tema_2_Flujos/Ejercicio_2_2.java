23b7b7adf9a04f6b55486fd6c43333f9731056e8
Anaroncero <146741422+Anaroncero@users.noreply.github.com>
2024-09-30 13:02:53 +0200

    Ejercicio_2_1 Acceso a datos

---
+++ Acceso a datos/Tema_2_Flujos/Ejercicio_2_2.java
@@ -0,0 +1,16 @@
+/* Ejemplo 2.2: Manipulación de Texto con CharArrayWriter y CharArrayReader
+
+Objetivo:
+Aprender a usar las clases CharArrayWriter y CharArrayReader en Java para escribir, modificar, y leer datos de texto en memoria. El objetivo es trabajar con flujos de caracteres sin utilizar archivos, simulando operaciones de entrada y salida.
+
+Instrucciones:
+Crea un programa en Java que utilice las clases CharArrayWriter y CharArrayReader.
+Escribe una cadena de texto en el CharArrayWriter, conviértela a un array de caracteres y modifícala. Por ejemplo, escribe la cadena "Hola amigo", modifica el array cambiando "amigo" por "amiga". Deberás cambiar el carácter en la posición 9 del array ('o') por 'a'.
+Usa el CharArrayReader para leer los caracteres modificados y mostrar el resultado en pantalla. */
+
+public class Ejercicio_2_2 {
+    public static void main(String[] args) {
+        
+
+    }
+}
